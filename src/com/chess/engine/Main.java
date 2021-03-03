102f947050dfa0351f7c17e9999576a83111f6e9
winny290198 <cam290198@gmail.com>
2021-03-02 20:30:25 -0500

    Initial commit

---
+++ src/com/chess/engine/Main.java
@@ -0,0 +1,11 @@
+package com.chess.engine;
+
+import com.chess.gui.Table;
+
+public class Main {
+    public static void main(final String[] args) throws Exception {
+        Table.get().show();
+    }
+}
+
+
